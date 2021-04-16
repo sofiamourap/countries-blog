14eeab4697abf86963c97648b38a3f4dae9854c6
Sofia Moura <sofiamourap@gmail.com>
2021-04-16 20:43:14 +0100

    set up FE connected with BE

--- server/resolvers/post.js
+++ server/resolvers/post.js
@@ -1,9 +1,29 @@
 const { gql } = require("apollo-server-express");
+const { posts } = require("../temp");
 
-const totalPosts = () => 42;
+//queries
+const totalPosts = () => posts.length;
+const allPosts = () => posts;
+
+//mutations
+const newPost = (_, args) => {
+  // console.log(args);
+  const { title, description } = args.input;
+  const post = {
+    id: posts.length + 1,
+    title,
+    description,
+  };
+  posts.push(post);
+  return post;
+};
 
 module.exports = {
   Query: {
     totalPosts,
+    allPosts,
+  },
+  Mutation: {
+    newPost,
   },
 };
---
+++ server/temp.js
@@ -0,0 +1,21 @@
+const posts = [
+  {
+    id: 1,
+    title: "Post 1",
+    description: "post 1 description",
+  },
+  {
+    id: 2,
+    title: "Post 2",
+    description: "post 2 description",
+  },
+  {
+    id: 3,
+    title: "Post 3",
+    description: "post 3 description",
+  },
+];
+
+module.exports = {
+  posts,
+};

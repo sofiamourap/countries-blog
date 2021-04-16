cc577f1bcfc257965377ec0dbbb9edfe1c0e73b9
Sofia Moura <sofiamourap@gmail.com>
2021-04-16 20:43:14 +0100

    set up FE connected with BE

--- server/typeDefs/posts.js
+++ server/typeDefs/posts.js
@@ -1,7 +1,23 @@
 const { gql } = require("apollo-server-express");
 
 module.exports = gql`
+  type Post {
+    id: ID!
+    title: String!
+    description: String!
+  }
+
   type Query {
     totalPosts: Int!
+    allPosts: [Post!]!
+  }
+
+  input PostInput {
+    title: String!
+    description: String!
+  }
+
+  type Mutation {
+    newPost(input: PostInput!): Post!
   }
 `;
